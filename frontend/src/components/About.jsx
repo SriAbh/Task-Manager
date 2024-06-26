0bbb0a7bf42b635c1f280b247dce3c09d890ab5b
Abhishek <abhi221sri@gmail.com>
2024-06-27 00:51:05 +0530

    completed

---
+++ frontend/src/components/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./style.css"
+
+function About() {
+  return (
+    <div className='container about-class'>
+      <h2>This is About</h2>
+    </div>
+  )
+}
+
+export default About
